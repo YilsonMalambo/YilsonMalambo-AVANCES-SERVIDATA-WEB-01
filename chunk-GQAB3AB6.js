66d954db1c77dbdb7578f1820bc76518785fa2cb
alexis14kl <rapalexism@gmail.com>
2025-05-14 18:53:19 -0500

    tamaño predeterminado btn flotante whatsapp 1

---
+++ chunk-GQAB3AB6.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var t=[{path:"",redirectTo:"index",pathMatch:"full"},{path:"index",loadComponent:()=>import("./chunk-JDWGTVTD.js").then(o=>o.WelcomeComponent),data:{title:"Inicio"}},{path:"index/politicas-privacidad",loadComponent:()=>import("./chunk-LE2XE5GS.js").then(o=>o.PrivacyPoliciesComponent)}];export{t as routes};
