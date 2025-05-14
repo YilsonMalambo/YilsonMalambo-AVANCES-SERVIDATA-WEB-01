eb7d0f1acd1b3c300c240c4dc806180b4ce82aa9
alexis14kl <rapalexism@gmail.com>
2025-05-14 18:47:51 -0500

    add barra vertical de contactos

---
+++ chunk-K6EAKGSY.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var t=[{path:"",redirectTo:"index",pathMatch:"full"},{path:"index",loadComponent:()=>import("./chunk-C6VRAQGK.js").then(o=>o.WelcomeComponent),data:{title:"Inicio"}},{path:"index/politicas-privacidad",loadComponent:()=>import("./chunk-J7QNIFOR.js").then(o=>o.PrivacyPoliciesComponent)}];export{t as routes};
